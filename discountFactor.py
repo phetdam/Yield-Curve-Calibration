32792b10aa172374db363306810eb4cdfdcf07f6
excaeruleo <prototyped625@gmail.com>
2026-06-08 15:10:20 -0400

    Made changes to discountFactor.py to make discount factor calculations for each date's set of weeks and made a graphical representation of change in discount factor for each date

---
+++ discountFactor.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def calcDisFact(name: str):
+  df = pd.read_csv(name);
+  #print(df)
+  #dates = df['Date']
+  #print(dates)
+  weeks = [4, 6, 8, 13, 17, 26, 52]
+  periodDisFact = pd.DataFrame(index = df['Date'],  columns = weeks, dtype = float)
+  #print(periodDisFact)
+  for row in range(len(df)):  
+    for week in weeks:
+      rateCol = f"{week} WEEKS BANK DISCOUNT"
+      rate = df.loc[row, rateCol] / 100.0
+      t = week / 52.0
+      periodDisFact.loc[df.loc[row, 'Date'], week] = (1 / (1 * (1 + rate) ** t))
+  
+  return periodDisFact
+
+def graph(pd):
+  weeks = pd.columns
+  for date in pd.index:
+    plt.plot(weeks, pd.loc[date], marker = 'o', label = date)
+  
+  plt.xlabel('Maturity (Weeks)')
+  plt.ylabel('Discount Factor')
+  plt.title('Treasury Bill Discount Factors')
+  plt.legend()
+  plt.grid(True)
+  plt.show()
+
+def main():
+  periodDF = calcDisFact('daily-treasury-rates.csv')
+  print(periodDF)
+  graph(periodDF)
+
+if __name__ == "__main__":
+  main()
