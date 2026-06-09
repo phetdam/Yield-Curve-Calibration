bd1d7dd3010716a6ba142b58e2dc03765eb0fd1b
excaeruleo <prototyped625@gmail.com>
2026-06-08 21:42:55 -0400

    Changed file name of csv file and commit changes to discountFactor.py

--- discountFactor.py
+++ discountFactor.py
@@ -9,20 +9,20 @@ def calcDisFact(name: str):
   weeks = [4, 6, 8, 13, 17, 26, 52]
   periodDisFact = pd.DataFrame(index = df['Date'],  columns = weeks, dtype = float)
   #print(periodDisFact)
-  for row in range(len(df)):  
+  for row in range(len(df)):
     for week in weeks:
       rateCol = f"{week} WEEKS BANK DISCOUNT"
-      rate = df.loc[row, rateCol] / 100.0
+      rate = df.loc[row, rateCol] / 100.0;
       t = week / 52.0
       periodDisFact.loc[df.loc[row, 'Date'], week] = (1 / (1 * (1 + rate) ** t))
-  
+
   return periodDisFact
 
 def graph(pd):
   weeks = pd.columns
   for date in pd.index:
     plt.plot(weeks, pd.loc[date], marker = 'o', label = date)
-  
+
   plt.xlabel('Maturity (Weeks)')
   plt.ylabel('Discount Factor')
   plt.title('Treasury Bill Discount Factors')
@@ -31,7 +31,7 @@ def graph(pd):
   plt.show()
 
 def main():
-  periodDF = calcDisFact('daily-treasury-rates.csv')
+  periodDF = calcDisFact('daily-treasury-bill-rates.csv')
   print(periodDF)
   graph(periodDF)
 
