00cae3febd2ba4e8b70fe6ed4f0d5d9ecf8679ad
excaeruleo <prototyped625@gmail.com>
2026-06-26 21:31:50 -0400

    made changes to discountFactorParYield.py and added contents to __init__.py

--- excycc/__init__.py
+++ excycc/__init__.py
@@ -1,5 +1,13 @@
+import sys
+sys.path.insert(0, "../")
+from discountFactorParYield import *
+
 def main():
+  periodDF = calcDisFact('../daily-treasury-par-yield-curve-rates.csv')
+  print(periodDF)
+  graph(periodDF)
   pass
+  
 
 if __name__ == "__main__":
   main()
